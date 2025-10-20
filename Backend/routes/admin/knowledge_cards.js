e91945ccd17724dc8cf054008f928b7eb9977d01
Rainy May <1327282532@qq.com>
2025-10-20 15:14:00 +0800

    新增删除知识卡片接口

--- Backend/routes/admin/knowledge_cards.js
+++ Backend/routes/admin/knowledge_cards.js
@@ -40,5 +40,55 @@ router.get('/:id', async function(req, res){
         },
     });
 });
+
+/**
+ * 新建知识卡片
+ * POST /admin/knowledge_cards
+ */
+router.post('/', async function(req,res){
+    try{
+        const knowledgeCard = await KnowledgeCard.create(req.body);
+        res.status(201).json({
+            status: true,
+            message: '新建知识卡片成功',
+            data: knowledgeCard
+        });
+    }catch (error){
+        res.status(400).json({
+            status: false,
+            message: '新建知识卡片失败',
+            error: error.message
+        });
+    }
+});
+/**
+ * 删除知识卡片
+ * DELETE /admin/knowledge_cards/:id
+ */
+router.delete('/:id', async function (req, res){
+    try{
+        const { id } = req.params;
+        const knowledge_cards = await KnowledgeCard.findByPk(id);
+        if(knowledge_cards){
+            await knowledge_cards.destroy();
+            res.json({
+                status: true,
+                message: '删除知识卡片成功'
+            });
+        }else{
+            res.status(404).json({
+                status: false,
+                message: '知识卡片不存在'
+            })
+        }
+    }catch (error){
+        res.status(500).json({
+            status: false,
+            message: '删除知识卡片失败',
+            error: error.message
+        })
+    }
+})
+
 module.exports = router;
 
