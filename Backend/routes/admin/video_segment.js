cb7687115cbe4f7b7a63f94937337b712175af16
Rainy May <1327282532@qq.com>
2025-10-20 15:41:24 +0800

    新增查询视频分段详情接口

--- Backend/routes/admin/video_segment.js
+++ Backend/routes/admin/video_segment.js
@@ -19,4 +19,28 @@ router.get("/", async function (req, res) {
   });
 });
 
+/**
+ * 查询视频分段详情
+ * GET /admin/video_segments/:id
+ */
+router.get("/:id", async function (req, res) {
+  const { id } = req.params;
+  // 查询视频分段详情
+  const videoSegment = await VideoSegment.findByPk(id);
+  if (!videoSegment) {
+    return res.status(404).json({
+      status: false,
+      message: "视频分段不存在",
+    });
+  }
+  // 返回视频分段详情
+  res.json({
+    status: true,
+    message: "查询视频分段详情成功",
+    data: {
+      videoSegment,
+    },
+  });
+});
+
 module.exports = router;    
