422551eb9b8cf399edd59a10b1821b169fe4c3df
DIM1222 <13609725931@163.com>
2025-12-13 14:23:13 +0800

    为用户界面的按钮添加了图标

--- Frontend/src/views/UserPage.vue
+++ Frontend/src/views/UserPage.vue
@@ -469,10 +469,32 @@
             <path d="M8 8h8M8 12h8M8 16h5"/>
           </svg>视频库
         </button>
-        <button @click="saveProject" class="premium-action-btn save-btn">保存项目</button>
-        <button @click="exportProject" class="premium-action-btn export-btn">导出为可分享链接</button>
-        <button @click="openUploadModal" class="premium-action-btn upload-btn">上传视频</button>
-        <button @click="goToEditPage" class="premium-action-btn edit-cards-btn" :disabled="!currentVideo">编辑知识卡片</button>
+        <button @click="saveProject" class="premium-action-btn save-btn">
+          <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" style="margin-right: 2px; vertical-align: middle;">
+            <path d="M19 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11l5 5v11a2 2 0 0 1-2 2z"/>
+            <polyline points="17,21 17,13 7,13 7,21"/>
+            <polyline points="7,3 7,8 15,8"/>
+          </svg>保存项目
+        </button>
+        <button @click="exportProject" class="premium-action-btn export-btn">
+          <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" style="margin-right: 2px; vertical-align: middle;">
+            <path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"/>
+            <path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"/>
+          </svg>导出为可分享链接
+        </button>
+        <button @click="openUploadModal" class="premium-action-btn upload-btn">
+          <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" style="margin-right: 2px; vertical-align: middle;">
+            <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/>
+            <polyline points="17,8 12,3 7,8"/>
+            <line x1="12" y1="3" x2="12" y2="15"/>
+          </svg>上传视频
+        </button>
+        <button @click="goToEditPage" class="premium-action-btn edit-cards-btn" :disabled="!currentVideo">
+          <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" style="margin-right: 2px; vertical-align: middle;">
+            <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/>
+            <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"/>
+          </svg>编辑知识卡片
+        </button>
       </div>
       </main>
       
